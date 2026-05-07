b1d91fb53e0d91112e66cbac159f4672638b72ac
Joao Pedro <joaopedrogolenia@gmail.com>
2026-05-07 14:53:33 -0300

    aterei

--- frontend-acessibilidade/src/pages/Home.tsx
+++ frontend-acessibilidade/src/pages/Home.tsx
@@ -8,20 +8,6 @@ export function Home() {
 
   useEffect(() => {
     setLugares([
-      {
-        id: 1,
-        nome: 'ESCOLA ELAY3424 ',
-        descricao: 'Escola não sei do que',
-        statusAcessibilidade: 'ACESSIVEL',
-        localizacao: {  
-          type: 'Point',
-            coordinates: [-51.306651, -26.426072],
-        },
-        temRampa: true,
-        temBanheiroAcessivel: true,
-        temElevador: false,
-        temPortaLarga: false,
-      },
             {
         id: 1,
         nome: 'COMÉRCIO DE PEÇAS MABE ',
