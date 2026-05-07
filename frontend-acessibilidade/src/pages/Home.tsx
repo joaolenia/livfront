7b1b34c61363f927547f9ce3d3ead17df96dfbec
EvelinAntonelli <evelin.antonelli@escola.pr.gov.br>
2026-05-07 14:55:55 -0300

    alterado

--- frontend-acessibilidade/src/pages/Home.tsx
+++ frontend-acessibilidade/src/pages/Home.tsx
@@ -10,7 +10,7 @@ export function Home() {
     setLugares([
             {
         id: 1,
-        nome: 'COMÉRCIO DE PEÇAS MABE E VETERINARIA',
+        nome: 'COMÉRCIO DE peças MABE E VETERINARIA',
         descricao: 'Provavelmente vende peçaj',
         statusAcessibilidade: 'PARCIALMENTE ACESSIVEL',
         localizacao: {  
